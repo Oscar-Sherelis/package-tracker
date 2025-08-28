97614a9aa36085f8e1eab70d6ef30603887a229a
Oscar <oskaras34@gmail.com>
2025-08-28 17:38:37 +0300

    fixed server settings files

--- server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c64adcaeef9907c7b55293c02706fe55b7c989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4e869487fae5c9c4340b937bde153d81b4d753")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
